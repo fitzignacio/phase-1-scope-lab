7da574726996692554780a8b8404b4eb72094ad2
Fitz Ignacio <ignacio.fitz@gmail.com>
2022-12-08 21:00:09 -0800

    commit tree 7ddddbf5c3873b7d53ddd40dcee49dce4d185dcb

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName = 'BOB';
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+
+}
+
+const leastFavoriteCustomer = 'fitz'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'daniel'; 
+
+}
